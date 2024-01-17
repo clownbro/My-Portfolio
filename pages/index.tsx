98b9f6ecbded6289d03ba0f25b80800eaa8c359a
Anup Paudel <anup.paudel1995@gmail.com>
2024-01-17 10:05:42 +0545

    added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ export default function home() {
   return (
     <>
       <Head>
-        <title>Anup | DevOps Engineer</title>
-        <meta name="description" content="I am a Devops Engineer With 7 years Experienced " />
+        <title>Anup | Analyst</title>
+        <meta name="description" content="I am a Analyst With 7 years Experienced " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/Anuplogo.ico" />
       </Head>
