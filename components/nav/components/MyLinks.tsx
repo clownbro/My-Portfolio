8f2d24d501a2cb42c41f9a05e794d5016bcd59b7
Anup Paudel <anup.paudel1995@gmail.com>
2024-01-17 10:09:47 +0545

    added

--- components/nav/components/MyLinks.tsx
+++ components/nav/components/MyLinks.tsx
@@ -29,7 +29,7 @@ export const MyLinks = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.1 }}
       >
-        <Link href="https://www.researchgate.net/profile/Anup-Neupane" target="_blank" rel="nofollow">
+        <Link href="https://www.researchgate.net/profile/Anup-Paudel-3" target="_blank" rel="nofollow">
         <FaResearchgate size="2.4rem" />
         </Link>
       </motion.span>
@@ -38,7 +38,7 @@ export const MyLinks = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.1 }}
       >
-        <Link href="https://medium.com/@neupane.Anup33" target="_blank" rel="nofollow">
+        <Link href="https://medium.com/@anup-paudel1995" target="_blank" rel="nofollow">
           <FaMedium size="2.4rem" />
         </Link>
       </motion.span>
@@ -47,7 +47,7 @@ export const MyLinks = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.2 }}
       >
-        <Link href="https://www.linkedin.com/in/Anup-neupane-50082091/" target="_blank" rel="nofollow">
+        <Link href="https://www.linkedin.com/in/paudelanup/" target="_blank" rel="nofollow">
           <FaLinkedin size="2.4rem" />
         </Link>
       </motion.span>
@@ -57,7 +57,7 @@ export const MyLinks = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.3 }}
       >
-        <Link href="https://www.instagram.com/makeajourneywithAnup" target="_blank" rel="nofollow">
+        <Link href="" target="_blank" rel="nofollow">
           <FaInstagram size="2.4rem" />
         </Link>
       </motion.span>
@@ -67,7 +67,7 @@ export const MyLinks = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.3 }}
       >
-        <Link href="https://project.neupaneAnup.com.np" target="_blank" rel="nofollow">
+        <Link href="https://anuppaud.com" target="_blank" rel="nofollow">
           <FaBlog size="2.4rem" />
         </Link>
       </motion.span>
